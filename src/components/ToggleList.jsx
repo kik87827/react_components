332ffaa0dc4224b25a4de6c965335d9535fdc08d
kik87827 <kik87827@gmail.com>
2025-10-22 13:24:00 +0900

    toggle 리스트 작업 완료

---
+++ src/components/ToggleList.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import styles from "./ToggleList.module.scss"
+
+export default function ToggleList({ datas }) {
+  const [activeArray, setActiveArray] = useState(() => new Array(datas.length).fill(false))
+
+  return (
+    <div className={styles.toggleList}>
+      {datas.map((item, index) => {
+        return (
+          <div className={styles.toggleItem} key={item.id}>
+            <a
+              href="#"
+              className={[styles.toggleHandle, activeArray[index] && styles.toggleHandleActive].filter(Boolean).join(" ")}
+              onClick={(e) => {
+                e.preventDefault()
+                let copyActiveArray = [...activeArray]
+                copyActiveArray[index] = !copyActiveArray[index]
+                setActiveArray(copyActiveArray)
+              }}
+            >
+              {item.title}
+            </a>
+            <div className={styles.toggleContentsActive} style={{ display: activeArray[index] ? "block" : "none" }}>
+              {item.content}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
--- src/pages/PlayGround.jsx
+++ src/pages/PlayGround.jsx
@@ -1,3 +1,26 @@
+import ToggleList from "../components/ToggleList"
+
 export default function PlayGround() {
-    return <p>PlayGround</p>
+  const toggleData = [
+    {
+      id: 1,
+      title: <p>토글아이템 샘플 1</p>,
+      content: <p>토글아이템 컨텐츠 컨텐츠 컨텐츠 컨텐츠 컨텐츠</p>,
+    },
+    {
+      id: 2,
+      title: <p>토글아이템 샘플 2</p>,
+      content: <p>토글아이템 컨텐츠 컨텐츠 컨텐츠 컨텐츠 컨텐츠2</p>,
+    },
+    {
+      id: 3,
+      title: <p>토글아이템 샘플 3</p>,
+      content: <p>토글아이템 컨텐츠 컨텐츠 컨텐츠 컨텐츠 컨텐츠3</p>,
+    },
+  ]
+  return (
+    <>
+      <ToggleList datas={toggleData} />
+    </>
+  )
 }
