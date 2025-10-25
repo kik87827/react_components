42d1dbff81c5f638d33f049a4285de5976acdf51
kik8727 <kik87827@gmail.com>
2025-10-25 23:29:28 +0900

    모달 디자인 완료료

--- src/components/ModalLayer.jsx
+++ src/components/ModalLayer.jsx
@@ -1,23 +1,25 @@
+import { useState } from "react"
+import styles from "./ModalLayer.module.scss"
+
 export default function ModalLayer() {
   return (
     <>
       <div className={styles.modalLayer}>
-        <div className={styles.modalTableWrap}>
-          <div className={styles.modalTable}>
-            <div className={styles.modalTd}>
-              <div class="modal-box-container">
-                <div class="modal-message-wrap">
-                  <p class="modal-message">모달 메시지가 나옵니다.</p>
-                </div>
-                <div class="modal-control-wrap">
-                  <a href="javascript:;" class="btn-modal-control">
-                    <span class="text-node">확인</span>
-                  </a>
-                  <a href="javascript:;" class="btn-modal-control">
-                    <span class="text-node">취소</span>
-                  </a>
-                </div>
+        <div className={styles.modalTable}>
+          <div className={styles.modalTd}>
+            <div className={styles.modalBox}>
+              <div className={styles.modalMessageWrap}>
+                <p className={styles.modalMessage}>모달 메시지가 나옵니다.</p>
               </div>
+              <div className={styles.modalControlWrap}>
+                <a href="#" className={styles.btnModalControl}>
+                  <span>확인</span>
+                </a>
+                <a href="#" className={`${styles.btnModalControl} ${styles.theme_2}`}>
+                  <span>취소</span>
+                </a>
+              </div>
+              <a href="#" className={styles.btnModalClose}></a>
             </div>
           </div>
         </div>
